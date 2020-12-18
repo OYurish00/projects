c4d2e7318556a2af261c4d42706897214884e7f8
OYurish00 <33738806+OYurish00@users.noreply.github.com>
2020-12-18 13:29:52 -0500

    Update BagelsGame.java

--- Bagels_YurishO/BagelsGame.java
+++ Bagels_YurishO/BagelsGame.java
@@ -12,13 +12,9 @@ public class BagelsGame
   
   public BagelsGame()
   {
-    secretNumber = getSecretNumber();
-    //System.out.println(secretNumber.length);
-    //System.out.println("inside const");
-    //for(int i = 0; i< secretNumber.length;i++)
-      //System.out.println(secretNumber[i]);
-    
+    secretNumber = getSecretNumber();    
   }
+  
   private int[] getSecretNumber()
   {
     int[] secretNumber = new int[3];
@@ -35,7 +31,6 @@ public class BagelsGame
       len--;
     }
     return secretNumber;
-
    }
   
    public int[] returnSecretNumber()
@@ -45,13 +40,11 @@ public class BagelsGame
    
    public String getGuess(int resp)
    {
-     
-     //System.out.println(resp);
      int[] guess = new int[3];
      String strguess = Integer.toString(resp);
      for (int i = 0; i<3; i++)
      {
-       //then use index to put into array
+       //uses index to put into array
         char c = strguess.charAt(i);
         guess[i] = Integer.parseInt(String.valueOf(c));
      }
@@ -68,22 +61,23 @@ public class BagelsGame
          int isin = 0;
          int pos = 0;
          
+         //checks if number guessed is in the secret number
          int snum = guess[a];
          for (int b=0; b<3; b++)
          {
            if (snum - secretNumber[b] == 0)
            {
              isin =1;
-             //System.out.println("isin=1");
            }
          }
          
+         //checks if number guessed is in the same position of the secret number
          if (guess[a] - secretNumber[a] == 0)
            {
              pos=1;
-             //System.out.println("pos=1");
            }
-                 
+         
+         //determines output for each guessed number
          if (pos==1 && isin==1)
          {
            response += "Fermi ";
